7830448ccecce21cbd1636ccee70bff5c2a4ecbb
Shella <dimasshella04@gmail.com>
2021-07-09 19:29:46 +0700

    coba lagi

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,10 @@
 from main import application
+from flask import session
+
+sess = session()
+
 if __name__ == "__main__":
     application.secret_key = 'secret'
     application.config['SESSION_TYPE'] = 'filesystem'
+    sess.init_app(application)
     application.run()
