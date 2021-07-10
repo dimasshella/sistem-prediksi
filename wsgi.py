3de0256b221c02210580d7e4da25482c66b26a47
Shella <dimasshella04@gmail.com>
2021-07-10 13:42:34 +0700

    hm

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,6 @@
 from main import application
-from flask import session
-
-sess = session()
 
 if __name__ == "__main__":
     application.secret_key = 'resdust'
     application.config['SESSION_TYPE'] = 'filesystem'
-    sess.init_app(application)
     application.run()
